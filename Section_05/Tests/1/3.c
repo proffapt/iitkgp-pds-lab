06c2427404ed810749e0c52651a3c9651a991a8e
proffapt <proffapt@pm.me>
2022-05-14 13:08:40 +0530

    updating limits

--- Section_05/Tests/1/3.c
+++ Section_05/Tests/1/3.c
@@ -9,12 +9,8 @@ int main()
 		printf("Enter number of lines: "); scanf("%d", &n);
 
 		// Imposing constraints on the values of s and n:
-		if(s<=1 || s>=3) {
-			printf("value of s must be within [1,3]\n");
-		}
-		if(n<=1 || n>=6){ 
-			printf("value of n must be within [1,6]\n");
-		}
+		if(s<=1 || s>=6) printf("value of s must be within [1,6]\n");
+		if(n<=1 || n>=10) printf("value of n must be within [1,10]\n");
 
 		// exiting loop when required values are within range
 		if(s>=1 && s<=3 && n>=1 && n<=6) break;
