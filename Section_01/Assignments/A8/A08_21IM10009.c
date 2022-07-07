7242724f960f72cc3abc8db7c816f37816285d85
proffapt <proffapt@pm.me>
2022-07-07 20:44:59 +0530

    new and better patterns

--- Section_01/Assignments/A8/A08_21IM10009.c
+++ Section_01/Assignments/A8/A08_21IM10009.c
@@ -16,105 +16,88 @@
 int size;
 
 void patternOne(int a[][size]){
-	for (int i=0; i<size; i++){
-		for (int j=0; j<size; j++){
-			if(i==j) a[i][j]=1;
+	// circle/ellipse in middle
+	for (int i=0; i<size; i++)
+		for (int j=0; j<size; j++)
+			if(i==0 || j==0 || i==size-1 || j==size-1 || 
+				(i==1 && j==1) || (i==1 && j==size-2) || (i==size-2 && j==1) || (i==size-2 && j==size-2) ||
+				 (j>1 && j<size-2 && i>1 && i<size-2)) a[i][j]=1;
 			else a[i][j]=0;
-		}
-	}
 }
 
 void patternTwo(int a[][size]){
-	for (int i=0; i<size; i++){
-		for (int j=0; j<size; j++){
-			if(i+j==size || i+j==size-1 || i+j==1) a[i][j]=1;
+	// dikh jayega don't worry
+	for (int i=size-1; i>=0; i--)
+		for (int j=0; j<size; j++)
+			if((i+j<size-1 && i<size/2 && j<size/2) || 
+				(i+j>size-1 && i>=size/2 && j>=size/2 )) a[i][j]=1;
 			else a[i][j]=0;
-		}
-	}
 }
 
 // This is the logic for printing the arrays
-void printArray(int a[][size]){
-	// Traversing rows
-  	for(int i=0; i<size; i++){
-		// Traversing columns
-		for(int j=0; j<size; j++){
-			// Printing them beautifully
-			printf("  %d", a[i][j]);
-		}
+void printArray(int a[][size])
+  { // Traversing rows
+  	for(int i=0; i<size; i++)
+	  { // Traversing columns
+		for(int j=0; j<size; j++)
+		  { // Printing them beautifully
+			printf("  %d", a[i][j]); }
 		// New line after every row
-		printf("\n");
-	}
+		printf("\n"); }
 	// One more new line after the last line so that other printed things are separated
-	printf("\n");
-}
+	printf("\n"); }
 
 // This is the logic for bad encryption
-int BadEnc(int a[][size], int e[][size]){
-	// Genarating random key, same for every entry
+int BadEnc(int a[][size], int e[][size])
+  { // Genarating random key, same for every entry
 	int k=rand()%2;
 	// Traversing rows
-	for (int i=0; i<size; i++){
-		// Traversing columns
-		for (int j=0; j<size; j++){
-			// Logic for encryption.. badly..
-			e[i][j]=(a[i][j]+k)%2;
-		}
-	}
-	return k;
-}
+	for (int i=0; i<size; i++)
+	    // Traversing columns
+		for (int j=0; j<size; j++)
+		    // Logic for encryption.. badly..
+			e[i][j]=(a[i][j]+k)%2;  
+	return k; }
 
 // Decrypting the matrix encrypted via bad decryption
-void BadDec(int e[][size], int d[][size], int key){
-	// Traversing rows
-	for (int i=0; i<size; i++){
-		// Traversing columns
-		for (int j=0; j<size; j++){
-			// Logic for decryptiong after badly encrypting
-			d[i][j]=(e[i][j]+key)%2;
-		}
-	}
-}
+void BadDec(int e[][size], int d[][size], int key)
+  { // Traversing rows
+	for (int i=0; i<size; i++)
+	   // Traversing columns
+		for (int j=0; j<size; j++)
+		   // Logic for decryptiong after badly encrypting
+			d[i][j]=(e[i][j]+key)%2; }
 
 // This is the function for good encryption method
-int **GoodEnc(int a[][size], int e[][size]){
-	// generating random keys for every entry
+int **GoodEnc(int a[][size], int e[][size])
+  { // generating random keys for every entry
 	int **k;
 	k=(int **)calloc(size ,sizeof(int*));
-	for(int i=0;i<size ;i++){
+	for(int i=0;i<size ;i++)
 	  k[i]=(int *)calloc(size ,sizeof(int));
-	}
 	// Traversing rows
-	for (int i=0; i<size; i++){
+	for (int i=0; i<size; i++)
 		// Traversing columns
-		for (int j=0; j<size; j++){
+		for (int j=0; j<size; j++)
 			k[i][j]=rand()%2;
-		}
-	}
 	// Process of encryption
 	// Traversing rows
-	for (int i=0; i<size; i++){
+	for (int i=0; i<size; i++)
 		// Traversing columns
-		for (int j=0; j<size; j++){
+		for (int j=0; j<size; j++)
 			// Logic for encryption.. goodly?(idk the word.. hehe)
 			e[i][j]=(a[i][j]+k[i][j])%2;
-		}
-	}
 	
-	return k;
-}
+	return k; }
 
 // fucntion to decrypt the encrypted matrix.. encrypted via good encryption
-void GoodDec(int e[][size], int d[][size], int **k){
-	// Traversing rows
-	for (int i=0; i<size; i++){
+void GoodDec(int e[][size], int d[][size], int **k)
+  { // Traversing rows
+	for (int i=0; i<size; i++)
 		// Traversing columns
-		for (int j=0; j<size; j++){
+		for (int j=0; j<size; j++)
 			// Logic for decryption..
-			d[i][j]=(e[i][j]+k[i][j])%2;
-		}
-	}
-}
+			d[i][j]=(e[i][j]+k[i][j])%2; }
 
 int main(){
 	// seedint the rand() function so that we don't get unique random numbers on every subsequent run
@@ -141,8 +124,8 @@ int main(){
 	// code for menu which keeps running you command it to exit
 	int choice=0, badKey;
 	int **goodKey;
-	while(choice!=3){
-		printf("Enter 1 to play with pattern-1\n");
+	while(choice!=3)
+	  { printf("Enter 1 to play with pattern-1\n");
 		printf("Enter 2 to play with pattern-2\n");
 		printf("Enter 3 to exit\n\n");
 		printf("Enter your choice: "); scanf("%d", &choice);
@@ -186,8 +169,6 @@ int main(){
 			default: 
 				// The user is most probably dumb or smart, he might try to break the code?.. but let's handle the situation too
 				printf("\nInvalid choice chosen!\n\n");
-		}
-	}
-
+		} }
 	return 0;
 }
