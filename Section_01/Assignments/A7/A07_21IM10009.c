ae7df18c088a22f2f50282505112b898e2b66e40
proffapt <proffapt@pm.me>
2022-07-07 19:48:35 +0530

    fixing bug in printing: size of roll number array

--- Section_01/Assignments/A7/A07_21IM10009.c
+++ Section_01/Assignments/A7/A07_21IM10009.c
@@ -9,105 +9,82 @@
 #include<stdio.h>
 #include<string.h>
 
-typedef struct stud{
-	char name[19];
+typedef struct stud
+  { char name[20];
 	float marks[5];
-	char rollno[7];
-} student;
+	char rollno[10]; } student;
 
-void readlist(student list[], int size){
-	// reading the data for the structure
+void readlist(student list[], int size)
+  { // reading the data for the structure
     printf("\nEnter data for the structure\n\n");
-    for(int i=0; i<size; i++){
-		// getting names
+    for(int i=0; i<size; i++)
+	  { // getting names
         printf("Enter name(<19 characters): ");
-        scanf("%s", list[i].name);
-
+        scanf(" %[^\n]s", list[i].name);
 		// getting roll numbers
         printf("Enter roll number: ");
-        scanf("%s", list[i].rollno);
-
+        scanf(" %[^\n]s", list[i].rollno);
 		// getting marks
         printf("Enter the marks for the 5 subjects: ");
         for(int j=0; j<5; j++) scanf("%f", &list[i].marks[j]);
-		printf("----------------\n");
-    }	
-}
+		printf("----------------\n"); } }
 
-void printlist(student list[], int size){
-	// printing all the data form the structure
+void printlist(student list[], int size)
+  { // printing all the data form the structure
     printf("\nPrinting all the data\n\n");
-
-    for(int i=0;i<size;i++){
-		// printing names
-        printf("Name: ");
-        printf("%s",list[i].name);
-
+    for(int i=0;i<size;i++)
+	  { // printing names
+        printf("Name:");
+        printf(" %s", list[i].name);
 		// priting roll number
-        printf("\nRoll number: ");
-        printf("%s",list[i].rollno);
-
+        printf("\nRoll number:");
+        printf(" %s", list[i].rollno);
 		// printing marks
         printf("\nMarks for the 5 subjects: ");
         for(int j=0;j<5;j++) printf("%f,", list[i].marks[j]);
 		printf("\b \n");
-		printf("**************\n");
-    }
-}	
+		printf("**************\n"); } }	
 
-void sortlist(student list[], int size, int code){
-	// defining variable all at once
+void sortlist(student list[], int size, int code)
+  { // defining variable all at once
 	int i,j,k,check;
     student temp;
     float aggregate[2];
-
 	// implementing the switch based sorting algorithm
-    switch(code){
-        case 1: 
-            for(i=0;i<size;i++){
-                for(j=i;j<size;j++){
-                    check=strcmp(list[i].name,list[j].name);
-                    if(check>0){
-                        temp=list[i];
+    switch(code)
+	  { case 1: 
+            for(i=0;i<size;i++)
+			  { for(j=i;j<size;j++)
+				    { check=strcmp(list[i].name,list[j].name);
+                    if(check>0)
+					  { temp=list[i];
                         list[i]=list[j];
-                        list[j]=temp;
-                    }
-                }
-            }
+                        list[j]=temp; } } }
             break;
     	case 2: 
-            for(i=0;i<size;i++){
-                for(j=i;j<size;j++){
-                    check=strcmp(list[i].rollno,list[j].rollno);
-                    if(check>0){
-                        temp=list[i];
+            for(i=0;i<size;i++)
+			  { for(j=i;j<size;j++)
+				    { check=strcmp(list[i].rollno,list[j].rollno);
+                    if(check>0)
+					  { temp=list[i];
                         list[i]=list[j];
-                        list[j]=temp;
-                    }
-                }
-            }
+                        list[j]=temp; } } }
             break;
     	case 3: 
             aggregate[0]=aggregate[1]=0;
-            for(i=0;i<size;i++){
-                for(j=i;j<size;j++){
-                    for(k=0;k<5;k++) aggregate[0]+=list[i].marks[k];
-                    
+            for(i=0;i<size;i++)
+			  { for(j=i;j<size;j++)
+				  { for(k=0;k<5;k++) aggregate[0]+=list[i].marks[k];
                     for(k=0;k<5;k++) aggregate[1]+=list[j].marks[k];
-                    
-                    if(aggregate[0]>aggregate[1]){
-                        temp=list[i];
+                    if(aggregate[0]>aggregate[1])
+					  { temp=list[i];
                         list[i]=list[j];
-                        list[j]=temp;
-                    }
-                }
-            }
+                        list[j]=temp; } } }
             break;
-	}
-}
+		case 4:
+			break; } }
 
 int main(){
-	
 	int entries, code;
 	printf("Enter the number of entries in the list: "); scanf("%d", &entries);
 
@@ -116,9 +93,12 @@ int main(){
 	readlist(list, entries);
 	printlist(list, entries);
 
+	do{
+	printf("\n1) Name 2) Roll No 3) aggregate 4) exit");
 	printf("\nEnter the code to sort the dataset with: "); scanf("%d", &code);
 	sortlist(list, entries, code);
 	printlist(list, entries);
+	}while(code!=4);
 
 	return 0;
 }
