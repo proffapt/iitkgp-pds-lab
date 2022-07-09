c226685408f9d0f91b75fb74a613ef8df4e9e274
proffapt <proffapt@pm.me>
2022-07-09 10:56:55 +0530

    udapte usage limit implemented

--- Section_01/Assignments/A6/A06_21IM10009.c
+++ Section_01/Assignments/A6/A06_21IM10009.c
@@ -27,7 +27,7 @@ int main(){
 	// taking input for the number of entries
 	int n; printf("Please enter n: "); scanf("%d", &n);
 	// Declaring tables
-	int table1[23*n];
+	int table1[22*n];
 	char table2[14*n];
 	// Generating both the tables
 	generateTable1(table1, n);
@@ -65,8 +65,8 @@ int main(){
 				printf("Please enter the name: "); scanf(" %s", username);
 				printf("Please enter new Limit: "); scanf("%d", &newLimit);
 				updateLimit(username, newLimit, table1, table2, n);
-				/* printf("Updated "); */
-				/* printTable1(table1, n); */
+				printf("Updated ");
+				printTable1(table1, n);
 				break;
 			case 3: 	
 				/* unsigned long long int cardnumber; */
@@ -87,21 +87,28 @@ int main(){
 
 void updateLimit(char name[],int newlimit, int table1[], char table2[], int entries){
 	int found=1, len_name;
-	// getting length of name
-	for(int i=0, len_of_name=0, j=0; i<13*entries; i+=(10-len_of_name), len_of_name=j=0){
+	for(int i=0, len_of_name=0, j=0; i<13*entries; i+=(10-len_of_name), len_of_name=j=0, found=1){
 		i+=3;
+		// comparing data from input
 		do{
 			if(table2[i++]!=name[j++]) {
-				printf("didn't match: %c .. %c\n", table2[i-1], name[j-1]);
 				found=0;
 				break;
 			}
 			len_of_name++;
-		}while(table2[i]!='\n'); i--;
+		}while(table2[i]!='\n');
+		// getting length of name
 		for(len_name=0; name[len_name]!='\0'; len_name++) continue;
-		printf("len_name = %d, len_of_name=%d, found=%d\n", len_name, len_of_name, found);
 		if(len_name==len_of_name && found){
-			printf("milgya\n");
+			// updating the value of limit
+			int factor=(i-(len_of_name+3))/14;
+			int limit_index=21;
+			for( ; factor>0; factor--) limit_index+=22;
+			for(int count=0; count<3; count++){
+				table1[limit_index]=newlimit%10;
+				newlimit/=10;
+				limit_index--;
+			}
 			break;
 		}
 	}
@@ -114,9 +121,9 @@ void generateTable1(int table[], int entries){
 		// making sure the first number of credit card is not 0.
 		if (table[index]==0){
 			table[index]=1+rand()%9; // random numbers from 1 to 9.
-			index+=23;
+			index+=22;
 		}
-		else if(i==index) index+=23;
+		else if(i==index) index+=22;
 	}
 }
 
@@ -134,29 +141,30 @@ void printTable1(int table[], int entries){
 // Generating table number 2
 void generateTable2(char table[], int table1[], int entries){
 	// Copying ID from the table1 
-	for(int i1=0, i2=0; i2<13*entries; i1+=22, i2+=13)
-		for(int j=0; j<3; j++)
-			table[i2+j]=table1[i1+j];
+	for(int i1=0, i2=0; i2<13*entries; i1+=22, i2+=14)
+		for(int j=0; j<3; j++){
+			table[i2+j]=(char)table1[i1+j];
+		}
 	// Taking input from the user for usernames
 	char garbage; scanf("%c", &garbage);
-	for(int i=0, len_of_name=0; i<13*entries; i+=(10-len_of_name), len_of_name=0){
+	for(int i=0, len_of_name=0; i<13*entries; i+=(11-len_of_name), len_of_name=0){
 		printf("Enter username for ID %d%d%d: ", table[i++], table[i++], table[i++]);
 		do{
 			scanf("%c", &table[i++]);
 			len_of_name++;
-		}while(table[i-1]!='\n');
+		}while(table[i-1]!='\n'); len_of_name--; i--;
 	}
 }
 
 // Printing table number 2
 void printTable2(char table[], int entries){
 	printf("\nTable 2:\n");
-	for(int i=0, len_of_name=0; i<13*entries; i+=(10-len_of_name), len_of_name=0){
+	for(int i=0, len_of_name=0; i<13*entries; i+=(11-len_of_name), len_of_name=0){
 		printf("ID %d%d%d:  ", table[i++], table[i++], table[i++]);
 		printf(" Name: ");
 		do{
 			printf("%c", table[i++]);
 			len_of_name++;
-		}while(table[i-1]!='\n');
+		}while(table[i-1]!='\n'); len_of_name--; i--;
 	}
 }
